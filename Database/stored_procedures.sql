14f86bfce7ad8170e4da68712d8c15bc0ae168ef
Matias Paz <matiasdanielpaz@outlook.com.ar>
2026-05-21 18:33:39 -0300

    Creacion de procedimiento almacenado para crear rutinia general/personalizada y para modificar turina general/personalizada

--- Database/stored_procedures.sql
+++ Database/stored_procedures.sql
@@ -1,22 +1,90 @@
-USE TPI_Base_de_datos_II
+USE GestionGimnasio
 GO
 
-CREATE PROCEDURE sp_ejemplo
-    @ID INT
+
+--Crear y modificar las rutinas para los socios.
+
+--Crear rutinas generales
+CREATE PROCEDURE SP_Rutina_General
+    @NOMBRE VARCHAR(150)
 AS
 BEGIN
     BEGIN TRY
         BEGIN TRANSACTION;
-        
+            INSERT INTO Rutinas (Nombre, FechaCreacion) VALUES (@NOMBRE, GETDATE());
         COMMIT TRANSACTION;
-
         PRINT 'OK';
     END TRY
     BEGIN CATCH
-		ROLLBACK TRANSACTION;
-            
-        PRINT 'Error: ' + ERROR_MESSAGE();
+		IF @@TRANCOUNT > 0 ROLLBACK TRANSACTION;   
+        PRINT 'Error al crear rutina: ' + ERROR_MESSAGE();
     END CATCH
 END;
 
 GO
+
+--Crear rutinas personalizada
+CREATE PROCEDURE SP_Rutina_Personaliazda_Usuario
+    @NOMBRE VARCHAR(150),
+    @ID_USUARIO BIGINT
+AS
+BEGIN
+    BEGIN TRY
+        BEGIN TRANSACTION;
+            INSERT INTO Rutinas (Nombre,IdUsuario, FechaCreacion) VALUES (@NOMBRE, @ID_USUARIO, GETDATE());
+        COMMIT TRANSACTION;
+        PRINT 'OK';
+    END TRY
+    BEGIN CATCH
+		IF @@TRANCOUNT > 0 ROLLBACK TRANSACTION;   
+        PRINT 'Error al crear rutina personalizada: ' + ERROR_MESSAGE();
+    END CATCH
+END;
+
+GO
+
+
+--
+
+--Modificar rutina general
+CREATE PROCEDURE SP_Modificar_Rutina_General
+    @ID INT,
+    @NOMBRE VARCHAR(150)
+AS
+BEGIN
+    BEGIN TRY
+        BEGIN TRANSACTION;
+            UPDATE Rutinas SET Nombre = @NOMBRE WHERE ID = @ID;
+        COMMIT TRANSACTION;
+        PRINT 'OK';
+    END TRY
+    BEGIN CATCH
+		IF @@TRANCOUNT > 0 ROLLBACK TRANSACTION;   
+        PRINT 'Error al modificar la rutina: ' + ERROR_MESSAGE();
+    END CATCH
+END;
+
+GO
+
+--Modificar rutina personalizada
+CREATE PROCEDURE SP_Modificar_Rutina_Personaliazda_Usuario
+    @ID INT,
+    @ID_USUARIO BIGINT,
+    @NOMBRE VARCHAR(150)
+AS
+BEGIN
+    BEGIN TRY
+        BEGIN TRANSACTION;
+            UPDATE Rutinas SET Nombre = @NOMBRE, IdUsuario = @ID_USUARIO WHERE ID = @ID;;
+        COMMIT TRANSACTION;
+        PRINT 'OK';
+    END TRY
+    BEGIN CATCH
+		IF @@TRANCOUNT > 0 ROLLBACK TRANSACTION;   
+        PRINT 'Error al modificar la rutina personalizada: ' + ERROR_MESSAGE();
+    END CATCH
+END;
+
+GO
+
+
