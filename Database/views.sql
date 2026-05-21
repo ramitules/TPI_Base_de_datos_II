05845b67cedc80d0549a018bff40db18251d9dd5
Franco Wagenhöfer <franwagen.500@gmail.com>
2026-05-21 14:39:48 -0300

    Agregadas vistas de suscripciones por usuario e historial de sesiones

--- Database/views.sql
+++ Database/views.sql
@@ -1,7 +1,61 @@
-USE TPI_Base_de_datos_II
+USE GestionGimnasio
 GO
 
-CREATE VIEW vst_ejemplo AS
-    SELECT * FROM Ejemplo;
-    
+---------------------------------------------------------------------------------------------------------------------
+-- Vista de Suscripciones por Usuario
+
+-- La vista sirve para consultar información sobre las suscripciones de los usuarios del gimnasio,
+-- incluyendo el plan contratado, estado de la suscripción, fechas de vigencia y días restantes hasta el vencimiento.
+---------------------------------------------------------------------------------------------------------------------
+
+--CREATE VIEW VW_SuscripcionesUsuarios
+--AS
+SELECT
+    u.ID AS IDUsuario,
+    u.NombreCompleto,
+    u.Email,
+    p.Nombre AS 'Plan',
+    p.PrecioMensual,
+    se.Nombre AS EstadoSuscripcion,
+    s.FechaInicio,
+    s.FechaVencimiento,
+    CASE
+        WHEN s.FechaVencimiento < GETDATE() THEN 0
+        ELSE DATEDIFF(DAY, GETDATE(), s.FechaVencimiento)
+    END AS DiasRestantes
+FROM Suscripciones s
+INNER JOIN Usuarios u
+    ON s.IdUsuario = u.ID
+INNER JOIN Planes p
+    ON s.IdPlan = p.ID
+INNER JOIN SuscripcionesEstados se
+    ON s.IdEstado = se.ID
+
 GO
+
+----------------------------------------------------------------------------------
+-- Vista de Historial de Entrenamiento
+
+-- La vista sirve para consultar un resumen de las sesiones de entrenamiento realizadas por los usuarios del gimnasio,
+-- mostrando la rutina utilizada, fecha, horario y duración de cada entrenamiento.
+----------------------------------------------------------------------------------
+
+--CREATE VIEW VW_HistorialEntrenamientoUsuarios
+--AS
+SELECT
+    se.ID AS IDSesion,
+    u.NombreCompleto AS Usuario,
+    r.Nombre AS Rutina,
+    CAST(se.FechaHoraInicio AS DATE) AS FechaEntrenamiento,
+    CAST(se.FechaHoraInicio AS TIME) AS HoraInicio,
+    CAST(se.FechaHoraFin AS TIME) AS HoraFin,
+    DATEDIFF(MINUTE, se.FechaHoraInicio, se.FechaHoraFin)
+        AS DuracionEntrenamientoMinutos
+FROM SesionesEntrenamiento se
+INNER JOIN Usuarios u
+    ON se.IdUsuario = u.ID
+LEFT JOIN Rutinas r
+    ON se.IdRutina = r.ID
+
+GO
+
