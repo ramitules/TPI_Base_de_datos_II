dc4240255099d56407721319c26edc7bd8d6c5b0
Matias Paz <matiasdanielpaz@outlook.com.ar>
2026-06-20 16:44:01 -0300

    Vista para logueo

--- Database/views.sql
+++ Database/views.sql
@@ -66,11 +66,9 @@ GO
 -- La vista sirve para consultar todos los datos necesario para instanciar los distintos tipos de usuarios y a la vez para la consulta del logueo
 ----------------------------------------------------------------------------------
 CREATE VIEW VW_Usuarios AS
-SELECT U.IdUsuarios, U.Nombre, U.Apellido, U.Email, U.FechaNacimiento, U.PesoCorporalKG, U.IdRol, R.Nombre AS [Rol Nombre], U.FechaIngreso, S.IdPlan, P.Nombre AS [Plan], P.PrecioMensual, P.DuracionDias, S.IdEstado, SE.Nombre AS [Estado], S.FechaInicio, S.FechaVencimiento, AU.Pass FROM Usuarios U
+SELECT U.IdUsuarios, U.Nombre, U.Apellido, U.Email, U.FechaNacimiento, U.PesoCorporalKG, U.IdRol, R.Nombre AS [Rol Nombre], U.FechaIngreso, S.IdPlan, U.Activo, P.Nombre AS [Plan], P.PrecioMensual, P.DuracionDias, S.IdEstado, SE.Nombre AS [Estado], S.FechaInicio, S.FechaVencimiento, AU.Pass FROM Usuarios U
 LEFT JOIN Suscripciones S ON U.IdUsuarios = S.IdUsuario
 LEFT JOIN Planes P ON P.IdPlanes = S.IdPlan
 LEFT JOIN SuscripcionesEstados SE ON S.IdEstado = SE.IdSuscripcionesEstados
 LEFT JOIN Roles R ON U.IdRol = R.IdRoles
 LEFT JOIN AccesoUsuarios AU ON U.IdUsuarios = AU.IdUsuarios
-
-select * from VW_Usuarios
