3285e3526eae6c7df94626c137f912b39cce4c1a
Miguel <fernandezmonzonmiguel@gmail.com>
2026-06-21 00:48:22 -0300

    TRIGGERS: Agrego estructura (inicial) de Triggers a realizar.

--- Database/triggers.sql
+++ Database/triggers.sql
@@ -1,12 +1,32 @@
-USE TPI_Base_de_datos_II
+USE GestionGimnasio
 GO
 
-CREATE TRIGGER trg_ejemplo
-ON Ejemplo
+-- EN PREPARACION (MIGUE)
+-- Trigger para detectar un record personal y actualizar EsRecordPersonal.
+CREATE TRIGGER tr_DetectarRecordPersonal ON SeriesCompletadas
+AFTER INSERT
+AS
+BEGIN
+    -- A realizar domingo 22
+END;
+GO
+
+-- EN PREPARACION (MIGUE)
+-- Trigger para validar que los socios esten activos antes de iniciar una sesion de entrenamiento.
+CREATE TRIGGER tr_ValidarSesionConSuscripcionActiva ON SesionesEntrenamiento
+AFTER INSERT
+AS
+BEGIN
+    -- A realizar domingo 22
+END;
+GO
+
+-- EN PREPARACION (MIGUE)
+-- Trigger para validar que no se elija la misma rutina para el mismo socio el mismo dia.
+CREATE TRIGGER tr_ValidarRutinaMismoDia ON Rutinas
 AFTER INSERT, UPDATE
 AS
 BEGIN
-    PRINT 'Ejemplo';
+    -- A realizar domingo 22
 END;
-
 GO
