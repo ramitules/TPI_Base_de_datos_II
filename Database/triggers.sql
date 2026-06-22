cab270b24896ea02f6eb6008a80d456a63a23422
Miguel <fernandezmonzonmiguel@gmail.com>
2026-06-21 23:33:25 -0300

    TRIGGERS: tr_DetectarRecordPersonal agregado (Sin testear)

--- Database/triggers.sql
+++ Database/triggers.sql
@@ -3,11 +3,55 @@ GO
 
 -- EN PREPARACION (MIGUE)
 -- Trigger para detectar un record personal y actualizar EsRecordPersonal.
-CREATE TRIGGER tr_DetectarRecordPersonal ON SeriesCompletadas
-AFTER INSERT
+-- Regla de negocio: Si el Peso levantado empata el Record para el ejercicio se verifica las repeticiones.
+-- Observaciones: En vez de buscar por la bandera vamos directo a los datos.
+
+Create Trigger tr_DetectarRecordPersonal ON SeriesCompletadas
+After Insert 
 AS
 BEGIN
-    -- A realizar domingo 22
+
+    -- Declaro las variables que voy a utilizar
+
+    Declare @IdSerieCompletada INT;
+    Declare @IdSesion INT;
+    Declare @IdEjercicio INT;
+    Declare @Repeticiones SMALLINT;
+    Declare @PesoLevantadoKG SMALLINT;
+    Declare @IdUsuario INT; -- Lo voy a tomar de la Sesion de entrenamiento con el IdSesion
+
+    -- Capturo los datos insertados de SeriesCompletadas
+
+    Select  @IdSerieCompletada = IdSeriesCompletadas, 
+            @IdSesion = IdSesion, 
+            @IdEjercicio = IdEjercicio, 
+            @Repeticiones = RepeticionesLogradas, 
+            @PesoLevantadoKG = PesoLevantadoKG
+    From Inserted;
+
+    -- Capturo el Id del Socio que completo la serie
+
+    Select @IdUsuario = IdUsuario From SesionesEntrenamiento where IdSesionesEntrenamiento = @IdSesion;
+
+    -- Busco la serie record del Socio y capturo los valores.
+
+    Declare @MaxPesoLevantado SMALLINT;
+    Declare @MaxRepeticiones SMALLINT;
+
+    select top 1 
+        @MaxPesoLevantado = SC.PesoLevantadoKG,
+        @MaxRepeticiones = SC.RepeticionesLogradas
+    from SeriesCompletadas SC 
+    INNER JOIN SesionesEntrenamiento SE on SC.IdSesion = SE.IdSesionesEntrenamiento
+    WHERE SE.IdUsuario = @IdUsuario AND SC.IdEjercicio = @IdEjercicio AND SC.IdSeriesCompletadas != @IdSerieCompletada
+    ORDER BY SC.PesoLevantadoKG Desc, SC.RepeticionesLogradas DESC;
+
+    -- Chequear si es Record e insertar
+    IF @MaxPesoLevantado is NULL OR @PesoLevantadoKG > @MaxPesoLevantado or (@PesoLevantadoKG = @MaxPesoLevantado AND @Repeticiones > @MaxRepeticiones)
+    BEGIN
+        UPDATE SeriesCompletadas 
+        Set EsRecordPersonal = 1 where IdSeriesCompletadas = @IdSerieCompletada;
+    END
 END;
 GO
 
