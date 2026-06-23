ab2fbaa1e865acc0824039acec032dbc58703908
Franco Wagenhöfer <franwagen.500@gmail.com>
2026-06-23 20:19:31 -0300

    Implementación de funciones para cantidad y duración de entrenamientos

--- Database/functions.sql
+++ Database/functions.sql
@@ -1,5 +1,4 @@
-USE TPI_Base_de_datos_II
-GO
+USE GestionGimnasio
 
 CREATE FUNCTION fn_ejemplo ()
 RETURNS INT
@@ -19,5 +18,49 @@ AS
 BEGIN
     -- A realizar domingo 22
     RETURN 1;
+END;
+GO
+
+
+--  Función que devuelve la cantidad de sesiones de entrenamiento registradas por un usuario.
+CREATE FUNCTION fn_CantidadEntrenamientosUsuario
+(
+    @IdUsuario INT
+)
+RETURNS INT
+AS
+BEGIN
+
+    DECLARE @Cantidad INT;
+
+    SELECT
+        @Cantidad = COUNT(*)
+    FROM SesionesEntrenamiento
+    WHERE IdUsuario = @IdUsuario;
+
+    RETURN @Cantidad;
+
+END;
+GO
+
+
+--  Función que devuelve la cantidad total de minutos entrenados por un usuario en todas sus sesiones. 
+CREATE FUNCTION fn_MinutosEntrenadosUsuario
+(
+    @IdUsuario INT
+)
+RETURNS INT
+AS
+BEGIN
+
+    DECLARE @Minutos INT;
+
+    SELECT @Minutos =
+        SUM(DATEDIFF(MINUTE, FechaHoraInicio, FechaHoraFin))
+    FROM SesionesEntrenamiento
+    WHERE IdUsuario = @IdUsuario;
+
+    RETURN ISNULL(@Minutos, 0);
+
 END;
 GO
