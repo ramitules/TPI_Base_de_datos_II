4ee011fe9d727ba2fd8a85fee953814f04be6906
Miguel <fernandezmonzonmiguel@gmail.com>
2026-06-24 19:13:16 -0300

    FUNCTIONS: Se agregó fn_DiasRestantesSuscripcion

--- Database/functions.sql
+++ Database/functions.sql
@@ -17,9 +17,46 @@ CREATE FUNCTION fn_DiasRestantesSuscripcion (@IdUsuario INT)
 RETURNS INT
 AS
 BEGIN
-    -- A realizar domingo 22
-    RETURN 1;
+
+    declare @resultadoDiasRestantes INT;
+    declare @fechaVencimiento DATE;
+
+    SELECT @fechaVencimiento = FechaVencimiento 
+    from Suscripciones
+    where IdUsuario=@Idusuario 
+        AND IdEstado = 1
+        and getdate() BETWEEN FechaInicio and FechaVencimiento;
+
+    IF (@fechaVencimiento is NOT NULL)
+        BEGIN        
+            set @resultadoDiasRestantes = DATEDIFF (day, GETDATE(), @fechaVencimiento);
+        END
+    ELSE
+        BEGIN
+            set @diasRestantes = 0;
+        END
+
+    RETURN @diasRestantes;
 END;
 GO
 
-CREATE FUNCTION fn_
+CREATE FUNCTION fn_VerificarSuscripcionActiva(@IdUsuario INT)
+Return BIT
+AS
+BEGIN
+    DECLARE @resultadoBusqueda INT
+    DECLARE @resultado BIT
+
+    Select @resultadoBusqueda = COUNT (*) 
+    FROM Suscripciones 
+    where IdUsuario=@IdUsuario 
+        and IdEstado = 1 
+        and getdate() BETWEEN FechaInicio and FechaVencimiento;
+
+    if (@resultadoBusqueda >= 1)
+        set @resultado = 1
+    ELSE 
+        set @resultado = 0;
+
+    return @resultado
+END
